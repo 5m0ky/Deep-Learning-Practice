8a1c1aea90d013cd9c4f338e6f45062f2458a29e
5m0ky <58104861+5m0ky@users.noreply.github.com>
2020-10-08 01:32:24 +0530

    Update meanSquaredError.py

--- meanSquaredError.py
+++ meanSquaredError.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3  # Remove this if you want to run it on windows
+
 import numpy as np
 
 y_predicted = np.array([1,1,0,0,1])
 y_true = np.array([0.30,0.7,1,0,0.5])
-epsilon = 1e-15
 
 def findMeanAbsoluteError(y_true, y_predicted):
+    epsilon = 1e-15
     total_error = 0
+    # This is how you iterate through two arrays in one line.
     for i, j in zip(y_predicted, y_true):
         total_error = abs(i - j)
     print(f"Total Error: {total_error}")
