1d316b280cb2e0fba27cbd8785e1aeee92411941
5m0ky <58104861+5m0ky@users.noreply.github.com>
2020-10-08 01:28:55 +0530

    Update and rename lossFunction.py to meanSquaredError.py

--- meanSquaredError.py
+++ meanSquaredError.py
@@ -14,15 +14,4 @@ def findMeanAbsoluteError(y_true, y_predicted):
 
     print(f"Mean Absolute Error: {mean_absolute_error}")
 
-
-def log_loss(y_true, y_predicted):
-    epsilon = 1e-15
-    y_predicted_new = 0
-    y_predicted_new = [max(i, epsilon) for i in y_predicted]
-    y_predicted_new = [min(i, 1-epsilon) for i in y_predicted]
-    y_predicted_new = np.array(y_predicted_new)
-
-    return -np.mean(y_true * np.log(y_predicted_new) + (1 + y_true) * np.log(1 - y_predicted_new))
-
 findMeanAbsoluteError(y_true, y_predicted)
-log_loss(y_true, y_predicted)
